f390e53667c07a6e7fae4f6880235cb1b6354a1d
Reuven <rdarab@gmail.com>
2026-06-14 03:44:58 -0400

    delete_todo function

--- todo_web.py
+++ todo_web.py
@@ -8,15 +8,23 @@ def add_todo():
     todo = st.session_state["new_todo"] +'\n'
     todos.append(todo)
     functions.write_to_file(todos)
+    st.session_state["new_todo"] = ''
 
-
+def delete_todo(inx):
+    todos.pop(inx)
+    functions.write_to_file(todos)
+    del st.session_state[inx]
+    st.rerun()
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=index)
+    if checkbox:
+        # print(checkbox)
+        delete_todo(index)
 
 st.text_input("Enter your productivity here", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
